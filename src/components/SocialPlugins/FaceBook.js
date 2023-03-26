30903ff28fe028799415a0d8e92a370e9ab3b442
Aman Sharma <aman703jk@gmail.com>
2023-03-27 01:57:32 +0530

    Added title to fb map

--- src/components/SocialPlugins/FaceBook.js
+++ src/components/SocialPlugins/FaceBook.js
@@ -4,6 +4,7 @@ const FaceBook = () => {
     return (
         <>
             <iframe
+                title='facebook'
                 className='wow fadeInUp'
                 src='https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FParamparaTheFest%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1185899982208393'
                 width='100%'
